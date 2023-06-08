5253cfb1d7a98edb7bd967d8adc00bd44005bdd8
sCuz12 <georgex8@gmail.com>
2023-06-08 19:53:05 +0300

    remove key

--- tests/Feature/VoicesTest.php
+++ tests/Feature/VoicesTest.php
@@ -5,7 +5,7 @@ use Georgehadjisavva\ElevenApiClient\Interfaces\ElevenClientInterface;
 use Georgehadjisavva\ElevenApiClient\Responses\ErrorResponse;
 
 beforeEach(function () {
-    $this->apiKey = 'cc2fc144a90d456a6d7c9deb202888b6'; // add your token
+    $this->apiKey = ''; // add your token
     $this->client = new ElevenApiClient($this->apiKey);
 });
 
